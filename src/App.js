9800cda5601f5ffc4ab032578420ffd176a7a5b0
Helios <hien.dnnt@gmail.com>
2020-05-06 13:00:59 +0700

    add props for TabMenu

--- src/App.js
+++ src/App.js
@@ -4,11 +4,33 @@ import FormLogin from './components/FormLogin';
 import Header from './components/Header';
 import TabMenu from './components/TabMenu';
 function App() {
+  const items = [
+    {
+      src: "https://image.flaticon.com/icons/png/512/1297/1297859.png",
+      label: "HOME"
+    },
+    {
+      src: "https://image.flaticon.com/icons/svg/548/548427.svg",
+      label: "DEALS"
+    },
+    {
+      src: "https://image.flaticon.com/icons/svg/1103/1103496.svg",
+      label: "UPLOAD"
+    },
+    {
+      src: "https://image.flaticon.com/icons/svg/633/633652.svg",
+      label: "WORK"
+    },
+    {
+      src: "https://image.flaticon.com/icons/svg/2099/2099058.svg",
+      label: "SETTINGS"
+    }
+  ]
   return (
     <div className="App">
       <Header/>
       <FormLogin/>
-      <TabMenu/>
+      <TabMenu items = {items}/>
     </div>
   );
 }
--- src/components/TabMenu.js
+++ src/components/TabMenu.js
@@ -3,48 +3,24 @@ import './TabMenu.css';
 
 class TabMenu extends Component {
   render() {
+    const { items } = this.props;
     return (
       <div class="tabMenu">
-        <div class="home">
-          <div>
-            <img alt="" src="https://image.flaticon.com/icons/png/512/1297/1297859.png" />
-          </div>
-          <div>
-            <span>HOME</span>
-          </div>
-        </div>
-        <div class="deals">
-          <div>
-            <img alt="" src="https://image.flaticon.com/icons/svg/548/548427.svg" />
-          </div>
-          <div>
-            <span>DEALS</span>
-          </div>
-        </div>
-        <div class="upload">
-          <div>
-            <img alt="" src="https://image.flaticon.com/icons/svg/1103/1103496.svg" />
-          </div>
-          <div>
-            <span>UPLOAD</span>
-          </div>
-        </div>
-        <div class="work">
-          <div>
-            <img alt="" src="https://image.flaticon.com/icons/svg/633/633652.svg" />
-          </div>
-          <div>
-            <span>WORK</span>
-          </div>
-        </div>
-        <div class="settings">
-          <div>
-            <img alt="" src="https://image.flaticon.com/icons/svg/2099/2099058.svg" />
-          </div>
-          <div>
-            <span>SETTINGS</span>
-          </div>
-        </div>
+      {
+        items.map(item => {
+          return (
+            <div class="item">
+              <div>
+                <img alt="" src={item.src} />
+              </div>
+              <div>
+                <span>{item.label}</span>
+              </div>
+            </div>
+          );
+        })
+      }
+      
       </div>
      );
   }
